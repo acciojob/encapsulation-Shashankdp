1f82d75cc07bcd05751957e4b82c869ef4a65ffa
shashank <shashankdpattar@gmail.com>
2023-01-15 00:04:44 +0530

    solution2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args){
         RWOnly obj=new RWOnly();
-        obj.set("shashank");
-        System.out.println(obj.get());
+        obj.setName("shashank");
+        System.out.println(obj.getName());
     }
   
 }
